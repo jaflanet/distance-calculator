57c9a2d9a744390bf1bfc97f6374788263ad05c7
Kevin Ezra <tkevinrocket@gmail.com>
2024-10-01 17:16:11 +0700

    add logic main

--- app/src/main/java/com/example/distancecalculator/MainActivity.kt
+++ app/src/main/java/com/example/distancecalculator/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.distancecalculator
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,6 +11,13 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 import com.example.distancecalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,21 +27,56 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+        val mySpinner: Spinner = findViewById(R.id.mySpinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.converter_lists,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            mySpinner.adapter = adapter
         }
+
+        val input = findViewById<EditText>(R.id.raw_distance)
+        val convertButton = findViewById<Button>(R.id.submit_btn)
+        convertButton.setOnClickListener() {
+            val inputDouble = input.text.toString().toDoubleOrNull()
+            if(inputDouble != null) {
+                val selected = mySpinner.selectedItem.toString()
+                if(selected == "KM to Miles") {
+                    val res = inputDouble * 0.621
+                    findViewById<TextView>(R.id.result_tv).text = "Result: ${res.toString()} Miles"
+                } else {
+                    val res = inputDouble * 1.609
+                    findViewById<TextView>(R.id.result_tv).text = "Result: ${res.toString()} Kilometres"
+                }
+            } else {
+                Toast.makeText(this, "Invalid input!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        input.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // No implementation needed for this method
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Check if s is not null and not empty
+                convertButton.isEnabled = !s.isNullOrEmpty()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // No implementation needed for this method
+            }
+        })
+
+//        setSupportActionBar(binding.toolbar)
+
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
